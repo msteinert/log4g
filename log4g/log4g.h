5b119369cd58a33a2000c1b15ecef0e326998c27
Michael Steinert <mike.steinert@gmail.com>
2010-06-02 16:36:30 -0600

    Updated build for release
    * add -std=gnu99 in a portable way
    * add -Wall in a portable way
    * added libtool versioning
    * removed dolt.m4
    * added AM_SILENT_RULES
    * see http://live.gnome.org/GnomeGoals/NicerBuilds
    
    Signed-off-by: Michael Steinert <mike.steinert@gmail.com>

--- log4g/log4g.h
+++ log4g/log4g.h
@@ -155,23 +155,6 @@
  *
  * \see \ref log4g/log4g.h "The Log4g core API"
  *
- * \section license Copyright & License
- *
- * Copyright 2010 Michael Steinert
- *
- * Log4g is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * Log4g is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Log4g. If not, see <http://www.gnu.org/licenses/>.
- *
  * \section references References
  *
  * -# <a href="http://logging.apache.org/log4j/1.2/manual.html">
@@ -194,6 +177,23 @@
  * - The <a href="http://www.gtk.org/development.html#Team">GLib/GObject
  *   development team</a>
  * for their fantastic work.
+ *
+ * \section license Copyright & License
+ *
+ * Copyright 2010 Michael Steinert
+ *
+ * Log4g is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * Log4g is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Log4g. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LOG4G_H
